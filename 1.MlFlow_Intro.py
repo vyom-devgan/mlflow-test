8ccccf188579078b568facb630ca47ee30c7cbc5
Vyom Devgan <80549269+vyom-devgan@users.noreply.github.com>
2023-11-26 10:29:04 -0500

    changes

--- 1.MlFlow_Intro.py
+++ 1.MlFlow_Intro.py
@@ -20,13 +20,11 @@ mlflow.set_tracking_uri(remote_server_uri)
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
-
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
-
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
@@ -73,8 +71,7 @@ if __name__ == "__main__":
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
-
-
+        
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
